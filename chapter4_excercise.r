25a22a858c14ddc51c8565c2cf1a70d18ce968d4
toparvia <toparvia@users.noreply.github.com>
2017-11-26 17:24:19 +0200

    Ex4.1part_completed

--- chapter4_excercise.r
+++ chapter4_excercise.r
@@ -11,12 +11,13 @@ rm(list=ls())
 #setwd(sourceDir)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-require(easypackages) # to download and load packages fast
+require("easypackages") # to download and load packages fast
+# install.packages("easypackages")
 # install.packages("stingi", dependencies = TRUE) #these are needed for corrplot
 # install.packages("broom", dependencies = TRUE) #these are needed for corrplot
 
-packages_to_load <- c("broom", "dplyr", "MASS", "tidyverse", "corrplot", "ggplot2", "GGally")
-packages(packages_to_load, prompt = FALSE) # lock'n'load install/load
+packages_to_load <- c("broom", "dplyr", "MASS", "tidyverse", "corrplot", "ggplot2", "GGally", "caret")
+packages(packages_to_load, prompt = TRUE) # lock'n'load install/load
 libraries(packages_to_load) # load
 
 # load the data
@@ -25,11 +26,17 @@ data("Boston")
 # explore the dataset
 str(      Boston)
 summary(  Boston)
+dim(      Boston)
+
+# All are numeric variables except chas and rad. 
 
 # plot matrix of the variables
 pairs(Boston)
 
-# data has demographical data of boston including tax and other information.
+p <-ggpairs(Boston, mapping = aes(alpha = 0.3), lower = list(combo = wrap("facethist", bins = 20)))
+
+
+# data has demographical data of boston including tax and other information possibly linked to crime rates within the city.
 
 my_fn <- function(data, mapping, ...){
   p <- ggplot(data = data, mapping = mapping) +
@@ -50,32 +57,33 @@ cor_matrix
 # visualize the correlation matrix
 corrplot(cor_matrix, method="circle", type = "upper", cl.pos = "b", tl.pos = "d", tl.cex = 0.6)
 
+# It seems that indus, nox, rad, tax and lstat have positive correlation with crime, and dis, black and medv have negative correlation with crime.
+
+
 # Scaling the variables with mean and standard deviation with scale()
 
-Boss <- as.data.frame(scale(Boston))
+Boston_scaled <- as.data.frame(scale(Boston))
 
 # crim (per capita crime rate by town)
 
 # summary of the scaled crime rate
-summary(crime)
+summary(Boston_scaled$crim)
 
-# create a quantile vector of crim and print it
-bins <- quantile(boston_scaled$crim)
+# create a quantile vector of crime and print it
+bins <- quantile(Boston_scaled$crim)
 bins
 
-# create a categorical variable 'crime'
-crime <- cut(boston_scaled$crim, breaks = bins, include.lowest= TRUE, label = c("low","med_low","med_high", "high"))
+# create a categorical variable 'crim'
+crime <- cut(Boston_scaled$crim, breaks = bins, include.lowest= TRUE, label = c("low","med_low","med_high", "high"))
 
 # look at the table of the new factor crime
 table(crime)
 
 # remove original crim from the dataset
-boston_scaled <- dplyr::select(boston_scaled, -crim)
+Boston_scaled <- dplyr::select(Boston_scaled, -crim)
 
 # add the new categorical value to scaled data
-boston_scaled <- data.frame(boston_scaled, crime)
-
-# boston_scaled is available
+Boston_scaled <- data.frame(Boston_scaled, crime)
 
 # number of rows in the Boston dataset
 n <-nrow(Boston)
@@ -84,10 +92,10 @@ n <-nrow(Boston)
 ind <- sample(n,  size = n * 0.8)
 
 # create train set
-train <- boston_scaled[ind,]
+train <- Boston_scaled[ind,]
 
 # create test set
-test <- boston_scaled[-ind,]
+test <- Boston_scaled[-ind,]
 
 # save the correct classes from test data
 correct_classes <-test$crime
@@ -101,30 +109,53 @@ lda.fit <- lda(crime ~., data = train)
 # print the lda.fit object
 lda.fit
 
-# the function for lda biplot arrows
-lda.arrows <- function(x, myscale = 1, arrow_heads = 0.1, color = "red", tex = 0.75, choices = c(1,2)){
-  heads <- coef(x)
-  arrows(x0 = 0, y0 = 0, 
-         x1 = myscale * heads[,choices[1]], 
-         y1 = myscale * heads[,choices[2]], col=color, length = arrow_heads)
-  text(myscale * heads[,choices], labels = row.names(heads), 
-       cex = tex, col=color, pos=3)
-}
+# Here we see how the first linear discriminant explain 94.72 % of the between-group variance in the Boston dataset.
+# Also the rad = index of accessibility to radial highways, which had before negative correlation with crime, has relatively big parameter in the first discriminant.
 
-# target classes as numeric
-classes <- as.numeric(train$crime)
 
-# plot the lda results
-plot(classes, dimen = 2, col = classes, pch = classes)
-lda.arrows(lda.fit, myscale = 1)
 
-# lda.fit, correct_classes and test are available
+# the function for lda biplot arrows, I couldn't get this to work
+# lda.arrows <- function(x, myscale = 1, arrow_heads = 0.1, color = "red", tex = 0.75, choices = c(1,2)){
+#   heads <- coef(x)
+#   arrows(x0 = 0, y0 = 0, 
+#          x1 = myscale * heads[,choices[1]], 
+#          y1 = myscale * heads[,choices[2]], col=color, length = arrow_heads)
+#   text(myscale * heads[,choices], labels = row.names(heads), 
+#        cex = tex, col=color, pos=3)
+# }
+# 
+# # target classes as numeric
+# classes <- as.numeric(train$crime)
+# 
+# # plot the lda results
+# plot(classes, dimen = 2, col = classes, pch = classes)
+# lda.arrows(lda.fit, myscale = 2)
+
+# I used another package I found in Github
+
+install.packages("devtools")
+library(devtools)
+install_github("fawda123/ggord")
+library(ggord)
+# Example
+# ord <- lda(Species ~ ., iris, prior = rep(1, 3)/3)
+# ggord(ord, iris$Species)
+
+# own data plotted
+library("ggthemes")
+library("scales")
+
+g <- ggord(lda.fit, train$crime, ellipse_pro = 0.95, vec_ext = 2, alpha_el = 0.3, size = 2)
+g  + theme_tufte()  + geom_rangeframe()
+# We get the points only seperated in by the 1st LD and rad parameter in the high group.
 
 # predict classes with test data
 lda.pred <- predict(lda.fit, newdata = test)
 
 # cross tabulate the results
 table(correct = correct_classes, predicted = lda.pred$class)
+
+# in High crime quartile, we could predict all cases, but for other classes we didn't do so well.
 
 # euclidean distance matrix
 dist_eu <- dist(Boston)
@@ -144,11 +175,12 @@ km <-kmeans(Boston, centers = 4)
 # plot the Boston dataset with clusters
 pairs(Boston[6:10], col = km$cluster)
 
-# MASS, ggplot2 and Boston dataset are available
+# Rad and tax, provide good seperation
+
 set.seed(123)
 
 # determine the number of clusters
-k_max <- 10
+k_max <- 4
 
 # calculate the total within sum of squares
 twcss <- sapply(1:k_max, function(k){kmeans(Boston, k)$tot.withinss})
@@ -157,10 +189,10 @@ twcss <- sapply(1:k_max, function(k){kmeans(Boston, k)$tot.withinss})
 qplot(x = 1:k_max, y = twcss, geom = 'line')
 
 # k-means clustering
-km <-kmeans(Boston, centers = 2)
+km <-kmeans(Boston, centers = 3)
 
 # plot the Boston dataset with clusters
-pairs(Boston, col = km$cluster)
+pairs(Boston[6:10], col = km$cluster)
 
 
 # For comparison using Caret - package including also centering
@@ -182,7 +214,55 @@ fit
 
 m <- lm(crim ~ . , data=Boston)
 m2 <- lm(crim ~ age + black + dis +indus + lstat, data=Boston)
-AIC(m,m2)
+m3 <- lm(crim ~ dis +tax, data=Boston)
+m4 <- lm(crim ~ zn + nox + dis + rad + ptratio + black + 
+           lstat + medv, data = Boston)
+AIC(m,m2,m3, m4)
 par(mfrow=c(2,2))
-plot(m2)
+plot(m4)
 plot(m)
+### Boston Data set information
+
+# This data frame contains the following columns:
+# 
+#   crim
+# per capita crime rate by town.
+# 
+# zn
+# proportion of residential land zoned for lots over 25,000 sq.ft.
+# 
+# indus
+# proportion of non-retail business acres per town.
+# 
+# chas
+# Charles River dummy variable (= 1 if tract bounds river; 0 otherwise).
+# 
+# nox
+# nitrogen oxides concentration (parts per 10 million).
+# 
+# rm
+# average number of rooms per dwelling.
+# 
+# age
+# proportion of owner-occupied units built prior to 1940.
+# 
+# dis
+# weighted mean of distances to five Boston employment centres.
+# 
+# rad
+# index of accessibility to radial highways.
+# 
+# tax
+# full-value property-tax rate per \$10,000.
+# 
+# ptratio
+# pupil-teacher ratio by town.
+# 
+# black
+# 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town.
+# 
+# lstat
+# lower status of the population (percent).
+# 
+# medv
+# median value of owner-occupied homes in \$1000s.
